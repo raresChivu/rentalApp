23ca572c7b41b03ef96e494b5a9dfc5f17e80832
raresChivu <rares.chivu@vodafone.com>
2025-07-03 14:17:19 +0300

    added file-based routing to Intro.tsx

--- CarRentalApp/app/pages/Intro.tsx
+++ CarRentalApp/app/pages/Intro.tsx
@@ -1,8 +1,11 @@
 import {Text, View} from 'react-native';
 import { Button } from 'react-native';
+import { useRouter } from 'expo-router';
 export default function Intro() {
+    const router = useRouter();
     const goToLogin = () => {
         console.log("Navigating to Login");
+        router.push('/pages/Login');
     };
     return (
         <View>
